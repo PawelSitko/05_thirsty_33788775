805913654cb09b46d59ffc7ccec53939b843ae74
PawelSitko <pawelsitko90@gmail.com>
2025-11-06 16:34:12 +0000

    finished extension task

--- routes/main.js
+++ routes/main.js
@@ -2,20 +2,63 @@
 const express = require("express");
 const router = express.Router();
 
+
 // Define our shop data
 var shopData = {
   shopName: "The Steak Shop",
-  productCategories: ["Ribeye", "Sirloin", "T-Bone", "Fillet", "Rump"]
+  productCategories: ["Ribeye", "Sirloin", "T-Bone", "Fillet", "Rump"],
+
+  //new shops 
+  shops: [
+    {
+      location: "London",
+      manager: "Pawel Sitko",
+      address: "3 Oxford Street, London W1D 2LT"
+    },
+    {
+      location: "Manchester",
+      manager: "Borris Johnson",
+      address: "45 Deansgate, Manchester M3 2BB"
+    },
+    {
+      location: "Birmingham",
+      manager: "Ibrahim Kamara",
+      address: "78 Broad Street, Birmingham B15 1AU"
+    }
+  ]
 };
+
+
+//survey result
+router.post("/survey_result", (req, res) => {
+  const data = {
+    shopName: shopData.shopName,
+    first: req.body.first,
+    last: req.body.last,
+    email: req.body.email,
+    age: req.body.age,
+    drink: req.body.drink,
+    student: req.body.student ? "Yes" : "No"
+  };
+
+  
+  res.render("survey_result.ejs", data);
+});
+
 
 // Handle the main routes
 router.get("/", (req, res) => {
   res.render("index.ejs", shopData);
 });
 
+router.get("/survey", (req, res) => {
+  res.render("survey.ejs", shopData);
+});
+
 router.get("/about", (req, res) => {
   res.render("about.ejs", shopData);
 });
+
 
 router.get("/search", (req, res) => {
   res.render("search.ejs", shopData);
